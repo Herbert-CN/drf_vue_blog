8144074da0293e4fcb3719cb579298ccac0bc3fb
Herbert Zhou <herbert.zhou@sioux.asia>
2023-01-04 10:25:45 +0800

    upload frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,16 @@
+const {defineConfig} = require('@vue/cli-service')
+module.exports = {
+    devServer: {
+        proxy: {
+            // detail: https://cli.vuejs.org/config/#devserver-proxy
+            '/api': {
+                target: `http://127.0.0.1:8000/api`,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+    lintOnSave: false
+};
